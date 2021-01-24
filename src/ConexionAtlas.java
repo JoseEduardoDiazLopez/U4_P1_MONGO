4f992df79b068840cb668555bcc6c52d5813c23f
lalop <lalop@LAPTOP-GMUI6RS2>
2021-01-23 21:52:14 -0700

    PMONGO

---
+++ src/ConexionAtlas.java
@@ -0,0 +1,10 @@
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+
+public class ConexionAtlas extends MongoClient {
+    public ConexionAtlas(MongoClientURI direccionConexion){
+        super(direccionConexion);
+    }
+}
